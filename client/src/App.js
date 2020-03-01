ad54697bc27bafd9a020454e6d92d1e2537907fe
dwinatech <diwina1993@hotmail.com>
2020-03-01 22:01:51 +0000

    Update App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import styled from "@emotion/styled";
+import NavBar from "./components/NavBar";
+import Heading from "./components/Heading";
+import Wrapper from "./components/Wrapper";
+import "./App.css";
+
+const StyledHeader = styled.div``;
+
+const lists = [
+  { name: "الرئيسية", path: "/" },
+  { name: "عننا", path: "/about" },
+  { name: "اتصل بنا", path: "/contact" }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       نافزة السودان
-      </header>
-    </div>
+    <Wrapper>
+      <NavBar lists={lists} />
+      <StyledHeader>
+        <Heading>نافزة السودان</Heading>
+      </StyledHeader>
+    </Wrapper>
   );
 }
 
