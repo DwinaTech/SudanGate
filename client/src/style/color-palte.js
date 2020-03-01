eb32d7dc5067816edc3faf6d11e3274ce7242953
dwinatech <diwina1993@hotmail.com>
2020-03-01 22:02:30 +0000

    Create colours palte variables

---
+++ client/src/style/color-palte.js
@@ -0,0 +1,5 @@
+export const headingColor = '#ffffff';
+export const paragraphColor = '#c2c2c2';
+export const backgroundColor = '#100e17';
+export const subHeadingColor = '#7a7a8c';
+export const cardBackgroundColor = '#1f1c18';
